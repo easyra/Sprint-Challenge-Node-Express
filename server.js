04019c69ab9cbe47878895a70e9edb2d3c3555b5
ezra davis davis <ezra.rr.davis@gmail.com>
2018-10-05 09:25:19 -0700

    update + delete

--- server.js
+++ server.js
@@ -45,6 +45,46 @@ server.post('/api/projects', (req,res) => {
         })
     }).catch(err => console.log(err))
 })
+
+server.delete(`/api/actions/:actionId`, (req,res) => {
+    const {actionId} = req.params
+    actionModelDb.remove(actionId).then(() => {
+        actionModelDb.get().then(actions => {
+            res.json(actions)
+        })
+    })
+
+})
+
+server.delete(`/api/projects/:projectId`, (req,res) => {
+    const {projectId} = req.params
+    projectModelDb.remove(projectId).then(() => {
+        projectModelDb.get().then(projects => {
+            res.json(projects)
+        })
+    })
+})
+
+server.put(`/api/actions/:actionId`, (req,res) => {
+    const {actionId} = req.params
+    const {project_id, description, notes, completed} = req.body;
+    actionModelDb.update(actionId, {project_id, description, notes, completed}).then(() =>{
+        actionModelDb.get().then(actions => {
+            res.json(actions)
+        })
+    }) 
+})
+
+server.put(`/api/projects/:projectId`, (req,res) => {
+    const {projectId} = req.params
+    const {name, description, completed} = req.body
+    projectModelDb.update(projectId, {name, description,completed}).then(() => {
+        projectModelDb.get().then(projects => {
+            res.json(projects)
+        })
+    })
+})
+
 server.listen(port, err => {
     if(err) console.log(err)
     console.log(`Server is running on port: ${port}`);
